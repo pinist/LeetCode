718e27bd18b39f9ddfbca12f7b76a8949b672992
pinist <36695545+pinist@users.noreply.github.com>
2018-04-22 23:31:29 +0800

    2018_4_22 upd_0

---
+++ 1-100/126.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    bool one(string a, string b){
+        int cnt=0;
+        for(int i=0;i<a.size();i++) cnt+=(a[i]!=b[i]);
+        return cnt==1;
+    }
+    
+    queue<int > qu;    
+    vector<vector<string> > ans;
+    vector<string> tmp;
+    
+    string beginWord;
+    vector<string> wordList;
+    int end=-1;
+    vector<int>d;
+    vector<vector<int> >g;
+    
+    void bfs(){
+        d[end]=0;
+        qu.push(end);
+        while(!qu.empty()){
+            int u=qu.front(); qu.pop();
+            for(auto v:g[u]){
+                if(d[v]==-1) {
+                    qu.push(v);
+                    d[v]=d[u]+1;}
+            }
+        }
+        //return;
+    }
+    int n;
+    void dfs(int dep, int u){
+        tmp[dep]=(u==n?beginWord:wordList[u]);
+        if(u==end){
+            ans.push_back(tmp);
+            return;
+        }
+        
+        for(auto v:g[u]){
+            if(d[v]==d[u]-1) dfs(dep+1,v);
+        }
+        
+    }
+    
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList){
+        n=wordList.size();
+        this->wordList=wordList; this->beginWord=beginWord;
+        if(!n) return ans;
+        d.assign(n+1,-1);
+        g.assign(n+1,vector<int>());
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(one(wordList[i],wordList[j])){
+                    g[i].push_back(j);g[j].push_back(i);
+                }
+            }
+            if(one(wordList[i],beginWord)) {
+                g[n].push_back(i);g[i].push_back(n);
+            }
+            if(wordList[i]==endWord) end=i;
+        }
+        if(end==-1) return ans;
+        cout<<end<<endl;
+        bfs();
+        if(d[n]==-1) return ans;
+        tmp.assign(d[n]+1," ");
+        dfs(0,n);
+        return ans;
+    }
+        
+    
+};
