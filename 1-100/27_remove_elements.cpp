142c654437403177cc031d11c3bff52e08acf585
pinist <36695545+pinist@users.noreply.github.com>
2018-04-23 10:37:22 +0800

    2018_4_23_upd_4

---
+++ 1-100/27_remove_elements.cpp
@@ -0,0 +1,37 @@
+/*
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example 1:
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+It doesn't matter what you leave beyond the returned length.
+ */
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if (nums.begin() == nums.end()) return 0;
+        vector<int>::iterator itor;
+        for (itor = nums.begin(); itor + 1 != nums.end(); )
+        {
+            if (*(itor + 1) == val) {
+                nums.erase(itor + 1);
+            }
+            else {
+                itor++;
+            }
+        }
+        itor = nums.begin();
+        if (*itor == val) {
+            nums.erase(itor);
+            return nums.size();
+        }
+        return nums.size();
+    }
+};
