880bd06169322085913130a745f3250e139a2add
pinist <36695545+pinist@users.noreply.github.com>
2018-04-23 10:42:37 +0800

    2018_4_23_upd_6

---
+++ 1-100/69_Sqrt(x).cpp
@@ -0,0 +1,29 @@
+/*
+Implement int sqrt(int x).
+ */
+class Solution {
+
+public:
+    int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int start = 0, end = x;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (x / mid == mid) {
+                return mid;
+            }
+            else if (x / mid > mid) {
+                start = mid;
+            }
+            else {
+                end = mid;
+            }
+        }
+        if (x / end == end) {
+            return end;
+        }
+        return start;
+    }
+};
