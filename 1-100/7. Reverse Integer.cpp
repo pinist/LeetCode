b4f18f8acf4ca6615c795f3cc260d411ec66e144
pinist <36695545+pinist@users.noreply.github.com>
2018-05-02 23:45:50 +0800

    2018_5_2_upd_1

---
+++ 1-100/7. Reverse Integer.cpp
@@ -0,0 +1,13 @@
+Given a 32-bit signed integer, reverse digits of an integer.
+
+class Solution {
+public:
+    int reverse(int x) {
+        long res=0;
+        while(x){
+            res=res*10+x%10;
+            x/=10;
+        }
+        return(res<INT_MIN||res>INT_MAX)?0:res;
+    }
+};
