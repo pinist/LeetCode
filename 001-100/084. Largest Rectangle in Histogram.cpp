970aa857fe61438419ca49ef5038754bb4936ebb
pinist <36695545+pinist@users.noreply.github.com>
2018-06-08 23:30:28 +0800

    2018_6_8_upd_0

--- 001-100/084. Largest Rectangle in Histogram.cpp
+++ 001-100/084. Largest Rectangle in Histogram.cpp
@@ -1,6 +1,7 @@
 /*
 Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
  */
+solution 1：
 class Solution {
 public:
     int largestRectangleArea(vector<int>& h) {
@@ -24,5 +25,37 @@ public:
         for(int i=0;i<n;i++)
            ans=max(ans,h[i]*(right[i]-left[i]+1));
         return ans;
+    }
+};
+solution 2：
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& h) {
+        int ans=0;
+        int n=h.size();
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            if(st.empty()||h[i]>=st.top()) st.push(h[i]);
+            else{
+                int cnt=0;
+                while(!st.empty()&&h[i]<st.top()){
+                    cnt++;
+                    ans=max(st.top()*cnt,ans);
+                    st.pop();
+                }
+                while(cnt--){
+                    st.push(h[i]);
+                }
+                st.push(h[i]);
+            }
+        }
+        int cc=1;
+        while(!st.empty()){
+            ans=max(st.top()*cc,ans);
+            cc++;
+            st.pop();
+        }
+        return ans;
+        
     }
 };
