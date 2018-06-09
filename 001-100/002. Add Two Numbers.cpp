1dab90a96c838f2ceb766538cbf2c2b88914e217
pinist <36695545+pinist@users.noreply.github.com>
2018-06-09 16:54:28 +0800

    2018_6_9_upd_0

--- 001-100/002. Add Two Numbers.cpp
+++ 001-100/002. Add Two Numbers.cpp
@@ -86,3 +86,45 @@ public:
     }
 };
 
+solution 2：
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head=NULL,*now=NULL;
+        int tmp=0;
+        while(l1!=NULL||l2!=NULL)
+        {
+            if(head==NULL)
+                now=head=new ListNode(0);
+            else
+            {
+                now->next=new ListNode(0);
+                now=now->next;
+            }
+            now->val=tmp;
+            if(l1!=NULL)
+            {
+                now->val+=l1->val;
+                l1=l1->next;
+            }  
+            if(l2!=NULL)
+            {
+                now->val+=l2->val;
+                l2=l2->next;
+            }
+            tmp=now->val/10;
+            now->val%=10;
+        }
+        if(tmp)
+            now->next=new ListNode(tmp);
+        return head;
+    }
+};
