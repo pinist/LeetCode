726c3a87a488b347a64947d715b947c557e0152f
pinist <36695545+pinist@users.noreply.github.com>
2018-06-18 22:14:09 +0800

    2018_6_18_upd_0

---
+++ 801-900/852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,22 @@
+/*
+Let's call an array A a mountain if the following properties hold:
+A.length >= 3
+There exists some 0 < i < A.length - 1 such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
+Given an array that is definitely a mountain, return any i such that A[0] < A[1] < ... A[i-1] < A[i] 
+> A[i+1] > ... > A[A.length - 1].
+ */
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        int ans=0,n=A.size();
+        if(!n) return ans;
+        int f=0;
+        for(int i=0;i<n;i++){
+            if(A[i]>ans) {
+                f=i;
+                ans=A[i];
+            }
+        }
+        return f;
+    }
+};
