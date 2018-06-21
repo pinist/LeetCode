86f2741b724b48ff1ff24d9514029fe80d8e6b3f
pinist <36695545+pinist@users.noreply.github.com>
2018-06-21 23:50:28 +0800

    2018_6_21_upd_0

---
+++ 001-100/057. Insert Interval.cpp
@@ -0,0 +1,40 @@
+/*
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+You may assume that the intervals were initially sorted according to their start times.
+ */
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+bool cmp(const Interval &A, const Interval &B){
+    return A.start<B.start;
+}
+
+class Solution {
+public:
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+        intervals.push_back(newInterval);
+        int n=intervals.size();
+        vector<Interval> ans;
+        if(!n) return ans;
+        sort(intervals.begin(),intervals.end(),cmp);
+        int st=intervals[0].start,ed=intervals[0].end;
+        for(int i=1;i<n;i++){            
+            if(ed>=intervals[i].start){
+                ed=max(ed,intervals[i].end);                
+            }
+            else{                
+                ans.push_back(Interval(st,ed));
+                st=intervals[i].start;
+                ed=intervals[i].end;
+            }
+        }
+        ans.push_back(Interval(st,ed));
+        return ans;
+    }
+};
