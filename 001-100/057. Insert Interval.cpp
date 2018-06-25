136b0c3d9ee0126e1fb81ea077e6810a6ef9714a
pinist <36695545+pinist@users.noreply.github.com>
2018-06-25 22:51:31 +0800

    2018_6_25_upd_0

--- 001-100/057. Insert Interval.cpp
+++ 001-100/057. Insert Interval.cpp
@@ -1,7 +1,7 @@
 /*
 Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
-You may assume that the intervals were initially sorted according to their start times.
- */
+You may assume that the intervals were initially sorted according to their start times
+*/
 /**
  * Definition for an interval.
  * struct Interval {
@@ -11,6 +11,7 @@ You may assume that the intervals were initially sorted according to their start
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
+
 
 bool cmp(const Interval &A, const Interval &B){
     return A.start<B.start;
