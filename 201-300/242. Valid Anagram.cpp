5ab1e137614ee3cb0a9abaab1bcf8a4bb344e981
pinist <36695545+pinist@users.noreply.github.com>
2018-07-03 23:57:25 +0800

    2018_7_3_upd_0

--- 201-300/242. Valid Anagram.cpp
+++ 201-300/242. Valid Anagram.cpp
@@ -1,3 +1,17 @@
 /*
-
+Given two strings s and t , write a function to determine if t is an anagram of s.
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
  */
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> tt;
+        tt.assign(26,0);
+        for(auto c:s) tt[c-'a']++;
+        for(auto c:t) tt[c-'a']--;
+        for(auto i:tt) if(i!=0) return false;
+        return true;
+    }
+};
